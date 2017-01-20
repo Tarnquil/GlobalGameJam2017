c366d023797be84c7b9c04be83fe746924926be0
Richard Humphries <Tarnquil@Richards-MacBook-Pro.local>
2017-01-20 18:57:17 +0000

    init commit

---
+++ Assets/Code/AudioInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioInput : MonoBehaviour
+{
+	[SerializeField]
+	AudioSource src;
+
+	bool hasMic = false;
+	float timeSinceRestart;
+
+	// Use this for initialization
+	void Start()
+	{
+		hasMic = Microphone.devices.Length > 0;
+
+		if(!hasMic)
+		{
+			return;
+		}
+
+		while(Microphone.GetPosition(null) > 0)
+		{
+		}
+
+		src.clip = Microphone.Start(Microphone.devices[1], true, 10, 44100);
+		src.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+
+		//pause a little before setting clip to avoid lag and bugginess
+		if(Time.time - timeSinceRestart > 0.5f&&!Microphone.IsRecording(null))
+		{
+			src.clip = Microphone.Start(null, true, 10, 44100);
+
+			//wait until microphone position is found (?)
+			while(!(Microphone.GetPosition(null) > 0))
+			{
+			}
+
+			src.Play(); // Play the audio source
+		}
+
+	}
+}
