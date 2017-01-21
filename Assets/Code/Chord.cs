2c896749df26f0a631bd4bf2c29f1d0fe6359284
Richard Humphries <Tarnquil@Richards-MacBook-Pro.local>
2017-01-21 10:50:37 +0000

    Ther other part of that commit

---
+++ Assets/Code/Chord.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chord : MonoBehaviour
+{
+	[SerializeField]
+	RectTransform anchor1, anchor2;
+	[SerializeField]
+	LineRenderer line;
+
+	// Use this for initialization
+	void Start()
+	{
+		line.SetPosition(0, anchor1.position);
+		line.SetPosition(2, anchor1.position);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+//		line.SetPosition(1, line.GetPosition(2) += new Vector3(1, 0));
+	}
+}
